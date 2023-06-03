efe1ee4f2a8c9eb9251b4b7dee1d5e141e7d7904
Sajadsaju <103987236+Sajadsaju@users.noreply.github.com>
2023-06-03 16:15:53 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-use Carbon;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use DB;
+use Carbon\Carbon;
+
 
 class UserController extends Controller
 {
@@ -17,16 +18,58 @@ class UserController extends Controller
         //
     }
 
-    public function createTask(Request $request){
-        $inputTask = $request['Task']; // Input data
+    public function createTask(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'Task.title' => 'required|string',
+            'Task.des' => 'required|string',
+        ]);
+
+        // Validation failed
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Validation passed
+        $inputTask = $request->input('Task');
 
         $task = DB::table('todo')->insert([
-            'title'=>$inputTask['title'],
-            'description'=>$inputTask['des'],
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now(),
+            'title' => $inputTask['title'],
+            'description' => $inputTask['des'],
+            'created_at' => Carbon::now()
         ]);
+
+        return response()->json(['message' => 'Task created successfully'], 201);
     }
 
+    public function UpdateTask(Request $request,$id){
+
+
+        $validator = Validator::make($request->all(), [
+            'Task.title' => 'required|string',
+            'Task.des' => 'required|string',
+        ]);
+
+        // Validation failed
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Validation passed
+        $inputTask = $request->input('Task');
+
+        $task = DB::table('todo')->where('id',$id)->update([
+            'title' => $inputTask['title'],
+            'description' => $inputTask['des'],
+            'updated_at' => Carbon::now(),
+        ]);
+
+        return response()->json(['message' => 'Task update successfully'], 201);
+    }
+
+
+    }
+
+
     //
-}
+
